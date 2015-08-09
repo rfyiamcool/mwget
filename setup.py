1aceef3235a1030589ea8a506820e9a13f214dfa
芮峰云 <rfyiamcool@163.com>
2015-08-09 23:16:32 +0800

    push pypi

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = "mwget",
+    version = "0.0.1",
+    author = "ruifengyun",
+    author_email = "rfyiamcool@163.com",
+    description = "threading download file",
+    license = "MIT",
+    keywords = "wget threading",
+    url = "https://github.com/rfyiamcool/mwget",
+    packages = ['mwget'],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Topic :: Internet :: WWW/HTTP",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
