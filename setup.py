54ef22e19b22fa9cc03e084594a537c034c0d6dc
芮峰云 <rfyiamcool@163.com>
2015-09-06 19:12:27 +0800

    update

--- setup.py
+++ setup.py
@@ -6,21 +6,21 @@ def read(fname):
 
 setup(
     name = "mwget",
-    version = "3.0.0",
+    version = "3.1.0",
     author = "ruifengyun",
     author_email = "rfyiamcool@163.com",
     description = "support threading download file",
     license = "MIT",
-    keywords = "mwget support threading and break download fengyun",
+    keywords = ["mwget support threading and break download","fengyun"],
     url = "https://github.com/rfyiamcool/mwget",
     packages = ['mwget'],
     long_description = read('README.md'),
-    classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 2.7',
-            'Topic :: Internet :: WWW/HTTP',
-            'Topic :: System :: Archiving',
-        ],
+    classifiers = [
+         'Development Status :: 2 - Pre-Alpha',
+         'Intended Audience :: Developers',
+         'License :: OSI Approved :: MIT License',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3.0',
+         'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
